439ac6b250556db4884c7cae3511ccf3da7e68bc
Pavel <balobanov057@gmail.com>
2021-03-13 23:46:46 +0400

    update
    switching to jquery

--- index.js
+++ index.js
@@ -1,77 +1,61 @@
-let imgClass = document.getElementById('parentsImg').getElementsByTagName('img');
-var timer = setInterval(rightClick, 4000);
+var imgClass = document.getElementById('parentsImg').getElementsByTagName('img');
+var timer = setInterval(function() {
+    $('#btn-right').click();
+}, 4000);
 
 $(document).ready(function() {
-    imgClass[0].classList.add(0);
-    imgClass[0].style.zIndex = '10';
-    for (let i = 1; i < imgClass.length; i++) {
-        imgClass[i].style.zIndex = '0';
+    for (let i = 0; i < imgClass.length; i++) {
         imgClass[i].classList.add(i);
-        imgClass[i].style.display = 'none'
+        i != 0 ? imgClass[i].style.display = 'none' : false;
     }
 })
 
 function restartTimer() {
     clearInterval(timer);
-    timer = setInterval(rightClick, 4000);
+    timer = setInterval(function() {
+        $('#btn-right').click();
+    }, 4000);
 }
 
-function rightClick() {
+$('#btn-right').click(function() {
     restartTimer();
-    let temp;
     for (let i = 0; i < imgClass.length; i++) {
-        if (imgClass[i].style.zIndex != "0") {
+        if (imgClass[i].style.display != "none") {
             if (i < imgClass.length-1) {
-                temp = i;
+                $(`.${imgClass[i].className}`).animate({width: "toggle"}, 400, "linear");
                 $(`.${imgClass[++i].className}`).animate({width: "toggle", right: '0'}, 400, "linear", function() {
-                    imgClass[i].removeAttribute("style");
+                    imgClass[i].style.right = 'initial';
                 });
-                $(`.${imgClass[temp].className}`).animate({width: "toggle"}, 400, "linear", function() {
-                    imgClass[i].removeAttribute("style");
-                });
-                imgClass[temp].style.zIndex = '0';
-                imgClass[i].style.zIndex = '10';
             } else {
-                imgClass[0].style.zIndex = '10';
-                temp = i;
                 $(`.${imgClass[0].className}`).animate({width: "toggle", right: '0'}, 400, "linear", function() {
-                    imgClass[0].style.cssText = "z-index: 10;";
+                    imgClass[0].style.right = 'initial';
                 });
-                $(`.${imgClass[temp].className}`).animate({width: "toggle"}, 400, "linear", function() {
-                    imgClass[temp].style.zIndex = '0';
-                });
+                $(`.${imgClass[i].className}`).animate({width: "toggle"}, 400, "linear");
             }
             break;
         }
     }
-}
 
-function leftClick() {
+})
+
+$('#btn-left').click(function() {
     restartTimer();
-    let temp;
     for (let i = 0; i < imgClass.length; i++) {
-        if (imgClass[i].style.zIndex != "0") {
+        if (imgClass[i].style.display != "none") {
             if (i != 0) {
-                temp = i;
+                $(`.${imgClass[i].className}`).animate({width: "toggle", right: '0'}, 400, "linear", function() {
+                    imgClass[++i].style.right = 'initial';
+                });
                 $(`.${imgClass[--i].className}`).animate({width: "toggle"}, 400, "linear", function() {
                 });
-                $(`.${imgClass[temp].className}`).animate({width: "toggle", right: '0'}, 400, "linear", function() {
-                    imgClass[temp].removeAttribute("style");
-                    imgClass[temp].style.cssText = "display: none; z-index: 0;";
-                });
-                imgClass[i].style.zIndex = '10';
             }
             else {
-                temp = i;
-                $(`.${imgClass[imgClass.length-1].className}`).animate({width: "toggle"}, 400, "linear", function() {
-                });
+                $(`.${imgClass[imgClass.length-1].className}`).animate({width: "toggle"}, 400, "linear");
                 $(`.${imgClass[0].className}`).animate({width: "toggle", right: '0'}, 400, "linear", function() {
-                    imgClass[0].removeAttribute("style");
-                    imgClass[0].style.cssText = 'z-index: 0; display: none;';
+                    imgClass[0].style.right = 'initial';
                 });
-                imgClass[imgClass.length-1].style.cssText = 'z-index: 10;';
             }
             break;
         }
     }   
-}
+});
