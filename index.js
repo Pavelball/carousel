10903fe6dea9d971f1d632c6c352bd68692978a1
Pavel <balobanov057@gmail.com>
2021-03-17 00:48:11 +0400

    update
    new animation

--- index.js
+++ index.js
@@ -2,12 +2,21 @@ var imgClass = document.getElementById('parentsImg').getElementsByTagName('img')
 var timer = setInterval(function() {
     $('#btn-right').click();
 }, 4000);
+var contJpg = [
+    "media/egor-blintsov-morrslieb-rising.jpg",
+    "media/egor-blintsov-night-harbor.jpg",
+    "media/egor-blintsov-rox-to-eden.jpg",
+    "media/egor-blintsov-witcher-2.jpg",
+    "media/warShips.jpg",
+    "media/bart-tchorzewski-2-painted.jpg"
+]
 
-$(document).ready(function() {
-    for (let i = 0; i < imgClass.length; i++) {
+$(document).ready(function () {
+    for (let i = 0; i < imgClass.length; i++) 
         imgClass[i].classList.add(i);
-        i != 0 ? imgClass[i].style.display = 'none' : false;
-    }
+    $(`.${imgClass[1].className}`).attr('src', `${contJpg[0]}`);
+    $(`.${imgClass[0].className}`).css('margin-left', '-100%');
+    $(`.${imgClass[0].className}`).attr('src', `${contJpg[contJpg.length-1]}`);
 })
 
 function restartTimer() {
@@ -17,45 +26,74 @@ function restartTimer() {
     }, 4000);
 }
 
-$('#btn-right').click(function() {
+$('#btn-right').click(function () {
     restartTimer();
-    for (let i = 0; i < imgClass.length; i++) {
-        if (imgClass[i].style.display != "none") {
-            if (i < imgClass.length-1) {
-                $(`.${imgClass[i].className}`).animate({width: "toggle"}, 400, "linear");
-                $(`.${imgClass[++i].className}`).animate({width: "toggle", right: '0'}, 400, "linear", function() {
-                    $(`.${imgClass[i].className}`).css('right', '');
-                });
+    let temp = 0;
+    $('.btn').prop("disabled", true);
+    $('.right').css('transform', 'rotate(-10deg)');
+    for (let i = 0; i < contJpg.length; i++) {
+        if ($(`.${imgClass[1].className}`).attr("src") == contJpg[i]) {
+            if (i == contJpg.length - 1) {
+                $(`.${imgClass[1].className}`).css('margin-left', '-100%');
+                $(`.${imgClass[2].className}`).attr('src', `${contJpg[temp]}`);
             } else {
-                $(`.${imgClass[0].className}`).animate({width: "toggle", right: '0'}, 400, "linear", function() {
-                    $(`.${imgClass[0].className}`).css('right', '');
-                });
-                $(`.${imgClass[i].className}`).animate({width: "toggle"}, 400, "linear");
+                temp = i + 1;
+                $(`.${imgClass[1].className}`).css('margin-left', '-100%');
+                $(`.${imgClass[2].className}`).attr('src', `${contJpg[temp]}`);
             }
             break;
         }
     }
+    setTimeout(function() {
+        $('.right').css('transform', '')
+    }, 100)
 
+    setTimeout(function () {
+        $(`.${imgClass[1].className}`).attr('src', `${contJpg[temp]}`);
+        $(`.${imgClass[1].className}`).css({
+            'display': 'none',
+            'margin-left': '0%'
+        });
+        setTimeout(function() {
+            $(`.${imgClass[1].className}`).css('display', '');
+            $('.btn').prop("disabled", false);
+        }, 100); 
+    }, 1000);
 })
 
-$('#btn-left').click(function() {
+
+$('#btn-left').click(function () {
     restartTimer();
-    for (let i = 0; i < imgClass.length; i++) {
-        if (imgClass[i].style.display != "none") {
-            if (i != 0) {
-                $(`.${imgClass[i].className}`).animate({width: "toggle", right: '0'}, 400, "linear", function() {
-                    $(`.${imgClass[++i].className}`).css('right', '');
-                });
-                $(`.${imgClass[--i].className}`).animate({width: "toggle"}, 400, "linear", function() {
-                });
-            }
-            else {
-                $(`.${imgClass[imgClass.length-1].className}`).animate({width: "toggle"}, 400, "linear");
-                $(`.${imgClass[0].className}`).animate({width: "toggle", right: '0'}, 400, "linear", function() {
-                    $(`.${imgClass[0].className}`).css('right', '');
-                });
+    let temp = 0;
+    $('.btn').prop("disabled", true);
+    $('.left').css('transform', 'rotate(100deg)');
+    for (let i = 0; i < contJpg.length; i++) {
+        if ($(`.${imgClass[1].className}`).attr("src") == contJpg[i]) {
+            if (i == 0) {
+                temp = contJpg.length-1;
+                $(`.${imgClass[0].className}`).attr('src', `${contJpg[temp]}`);
+                $(`.${imgClass[0].className}`).css('margin-left', ``);
+            } else {
+                temp = i - 1;
+                $(`.${imgClass[0].className}`).css('margin-left', '');
+                $(`.${imgClass[0].className}`).attr('src', `${contJpg[temp]}`);
             }
             break;
         }
-    }   
-});
+    }
+    setTimeout(function() {
+        $('.left').css('transform', '')
+    }, 100)
+
+    setTimeout(function () {
+        $(`.${imgClass[1].className}`).attr('src', `${contJpg[temp]}`);
+        $(`.${imgClass[0].className}`).css({
+            'display': 'none',
+            'margin-left': '-100%'
+        });
+        setTimeout(function() {
+            $(`.${imgClass[0].className}`).css('display', '');
+            $('.btn').prop("disabled", false);
+        }, 100); 
+    }, 1000);
+})
